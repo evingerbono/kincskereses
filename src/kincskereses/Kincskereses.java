cc40eca53bd89feb08dfaa9457c200a878492668
evingerbono <bono0823@gmail.com>
2022-06-06 00:44:11 +0200

    jatek

---
+++ src/kincskereses/Kincskereses.java
@@ -0,0 +1,45 @@
+
+package kincskereses;
+
+import java.util.Scanner;
+
+
+public class Kincskereses {
+
+    
+    public static void main(String[] args) {
+        
+        lada();
+    }
+    
+
+    private static void lada() {
+        String [] ladak= new String [3];
+        ladak[0]="Arany";
+        ladak[1]="Ezüst";
+        ladak[2]="Bronz";
+        System.out.println("Van három ládám, az egzik egy hatalmas kincset rejt. Ezeket a feliratokat tartalmazzák a ládák:");
+        System.out.println("1.Arany láda: Én relytem a kincset.");
+        System.out.println("2.Ezüst láda: Nem én relytem a kincset.");
+        System.out.println("3.Bronz láda: Az arany láda hazudik.");
+        System.out.println("A három ládábol 2-hazudik és csak 1-mond igazat. Melyik ládámba van a kincs? ");
+        
+        tipp();
+    }
+
+    private static void tipp() {
+        Scanner sc = new Scanner(System.in);
+        int tippek = sc.nextInt();
+        
+        if (tippek==1) {
+            System.out.println("Rosszul gondolkoztál nem az arany ládaába vana kincs.");
+        }else if(tippek==2){
+            System.out.println("Gratulálok megtalálltad a kincset.");
+        }else if(tippek==3){
+            System.out.println("Rosszul gondolkoztál nem a bronz ládába van a kincs.");
+        }
+  
+   
+    
+    }
+}
